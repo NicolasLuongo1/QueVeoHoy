4d39ec4f4af4e0f02035fddad69e5509c0a20c77
TomasElia35 <eliagimeneztomas@gmail.com>
2025-11-17 20:47:26 -0300

    [1.0.0]Refactor(implement-guard)Add implementation in route handling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home';
 import { TmdbLogin } from './tmdb/tmdb-login/tmdb-login';
 import { MovieDetail } from './pages/movie-detail/movie-detail';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
 	{ path: 'login', component: TmdbLogin },
-	{ path: 'home', component: HomeComponent },
-	{path:'detail/:id', component:MovieDetail, title:'Detalle de pelicula'},
+	{ path: 'home', component: HomeComponent, canActivate: [authGuard] },
+	{path:'detail/:id', component:MovieDetail, title:'Detalle de pelicula', canActivate: [authGuard]},
 	{ path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
