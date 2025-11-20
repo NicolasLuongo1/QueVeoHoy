a33a41ded3e8b261c2914b0cd294948f1ddc3f39
TomasElia35 <eliagimeneztomas@gmail.com>
2025-11-20 17:56:59 -0300

    Add Footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,12 @@ import { Router, RouterOutlet } from '@angular/router';
 import { TmdbService } from './services/tmdb-service';
 import { Header } from "./components/header/header";
 import { AuthService } from './services/auth-service';
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
